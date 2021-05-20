9dd37b2cf5f6e292f09b1e87240b11c8654679ce
VigneshReddyJulakanti <73946941+VigneshReddyJulakanti@users.noreply.github.com>
2021-05-20 14:26:41 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,22 @@
+
+ <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("sql6.freemysqlhosting.net", "sql6413562", "TkKdJbjaRE", "sql6413562");
+
+$result = $conn->query("SELECT phoneNo1 FROM workersdetails");
+
+$outp = "[";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "[") {$outp .= ",";}
+    $outp .= '{"phoneNo1":"'  . $rs["phoneNo1"] . '",';
+   //  $outp .= '"City":"'   . $rs["City"]        . '",';
+   //  $outp .= '"Country":"'. $rs["Country"]     . '"}';
+}
+$outp .="]";
+
+$conn->close();
+
+echo($outp);
+?>
