c579a9a4f03f83f74f6af43c5e109bd4293a2328
VigneshReddyJulakanti <73946941+VigneshReddyJulakanti@users.noreply.github.com>
2021-05-20 14:43:56 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,9 +1,9 @@
+
+ <?php
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
 header('Access-Control-Allow-Origin: *');
 
- <?php
-header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 $conn = new mysqli("sql6.freemysqlhosting.net", "sql6413562", "TkKdJbjaRE", "sql6413562");
