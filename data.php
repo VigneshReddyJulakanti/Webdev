893d21ecdc4978541df62e6c26a6328cbabab932
VigneshReddyJulakanti <73946941+VigneshReddyJulakanti@users.noreply.github.com>
2021-05-20 14:43:02 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,3 +1,6 @@
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
+header('Access-Control-Allow-Origin: *');
 
  <?php
 header("Access-Control-Allow-Origin: *");
